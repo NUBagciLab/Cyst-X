9abde3e19c597c3f2da69625cdeac547948c75f9
Zilian4 <115503180+Zilian4@users.noreply.github.com>
2025-09-26 14:25:24 -0500

    Update setup.sh

--- deploy/pipeline/setup.sh
+++ deploy/pipeline/setup.sh
@@ -1,9 +1,7 @@
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
-git clone https://github.com/MIC-DKFZ/nnUNet.git
-cd nnUNet
-git checkout nnunetv1
-pip install -e .
-
+git clone https://github.com/NUBagciLab/PaNSegNet.git
+cd PaNSegNet/src
+python setup.py
 export nnUNet_raw_data_base="/data2/pyq6817/nnUNetv1/nnUNet_raw_data_base" 
 export nnUNet_preprocessed="/data2/pyq6817/nnUNetv1/nnUNet_preprocessed" 
 export RESULTS_FOLDER="/data2/pyq6817/nnUNetv1/nnUNet_trained_models"  
